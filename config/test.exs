552d0127a8e83b72e491c1afec882a0da0464c15
Bernhard Stoecker <bernhard.stoecker@recogizer.de>
2017-09-11 22:51:51 +0200

    Create Repo and add configurations

---
+++ config/test.exs
@@ -0,0 +1,20 @@
+use Mix.Config
+
+config :simple_repo_sample, SimpleRepoSample.Web.Endpoint,
+  http: [port: 8002],
+  debug_errors: true,
+  code_reloader: true,
+  check_origin: false,
+  url: [host: "localhost", port: 8002]
+
+# Do not include metadata nor timestamps in development logs
+config :logger, :console, level: :debug, format: "[$level] $message\n"
+
+config :simple_repo_sample, SimpleRepoSample.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  database: "simple_repo_test",
+  username: "simple_repo_test",
+  password: "exs#exs456",
+  hostname: "simple-repo-postgres-s",
+  port: "5432",
+  pool_size: 10
---
+++ lib/simple_repo_sample/repo.ex
@@ -0,0 +1,3 @@
+defmodule SimpleRepoSample.Repo do
+  use Ecto.Repo, otp_app: :simple_repo_sample
+end
