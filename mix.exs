136b947edf08529781bf93c8419df0f51c0ee227
Bernhard Stoecker <bernhard.stoecker@recogizer.de>
2017-09-11 22:51:06 +0200

    Set dependencies to latest versions

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule SimpleRepoSample.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
+      compilers: [:phoenix] ++ Mix.compilers,
       deps: deps()
     ]
   end
@@ -14,15 +15,21 @@ defmodule SimpleRepoSample.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      [
+        applications: [
+          :logger, :cowboy, :phoenix, :postgrex, :ecto
+        ],
+        mod: {SimpleRepoSample.Application, []}
+      ]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:simple_repo, "~> 0.1.0"},
-      {:phoenix, "~> 1.3" }
+      {:simple_repo, "~> 0.2.0-rc1"},
+      {:phoenix, "~> 1.3" },
+      {:phoenix_live_reload, "~> 1.0"}
 
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
